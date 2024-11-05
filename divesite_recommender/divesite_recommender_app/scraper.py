f9f95dcf5f99cec3e3c777863524e290974dd159
AliElGoharyC <aelgohary1@sheffield.ac.uk>
2024-11-06 01:00:29 +0200

    Scraper finished, set up SQLite db

--- divesite_recommender/divesite_recommender_app/scraper.py
+++ divesite_recommender/divesite_recommender_app/scraper.py
@@ -13,8 +13,36 @@ def extract_continent(div_id):
     return continent
 
 
+def extract_dive_types(country_link):
+    response = requests.get(country_link)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-def scrape_countries():
+    dive_types = []
+    dive_type_div = soup.select('.info .diving-types span')
+
+    for div in dive_type_div:
+        dive_types.append(div.text.strip())
+
+    return dive_types
+
+def extract_logged_species(country_link):
+    response = requests.get(country_link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    logged_species = None
+    
+    logged_species_div = soup.select('.col-md-16 .block.counter-text')
+    for div in logged_species_div:
+        label = div.select_one('.small.text-uppercase.block.font-weight-light')
+        if label and 'Logged Species' in label.text:
+            count_span = div.select_one('.count')
+            if count_span:
+                logged_species = int(count_span.text.strip())
+            break
+
+    return logged_species
+
+def scrape_data():
     url = f'{BASE_URL}/destinations/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -26,9 +54,8 @@ def scrape_countries():
     for div in cont_div:
 
         div_id= div['id']
-        continent = extract_continent(div_id)
 
-        country_links.append(div_id)
+        continent = extract_continent(div_id)
 
         country_items = div.select('.destinations .title a')
 
@@ -36,12 +63,51 @@ def scrape_countries():
             country_name = country.text.strip()
             country_link = country['href']
 
+            # Get dive types
+            dive_types = extract_dive_types(country_link)
+            # Get logged species
+            logged_species = extract_logged_species(country_link)
+
             country_links.append({
-                'continent': continent,
-                'country': country_name,
-                'link': country_link
+                'continent':continent,
+                'country':country_name,
+                'link':country_link,
+                'Dive Types': dive_types,
+                'Logged Species': logged_species
             })
     return country_links
 
+
 if __name__ == '__main__':
-    print(scrape_countries())
+    print(scrape_data())
+
+# TO BE ADDED ITEMS
+
+    # def scrape_dive_details(country_link):
+#     response = requests.get(country_link)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+
+#     dive_details = {
+#         'General Info': extract_content(soup, 'general-info'),
+#         'Diving Info': extract_content(soup, 'diving-info')
+#     }
+
+
+#     # general_info_header = soup.find('h1', id='general-info')
+#     # if general_info_header:
+#     #     content = general_info_header.find_next_sibling('div', class_ = 'info-text')
+#     #     if content:
+#     #         dive_details['General Info'] = content.text.strip()
+    
+#     # # dive_season = TO BE ADDED
+
+#     return dive_details
+
+"""
+def extract_weather_conditions(country_link):
+    response = requests.get(country_link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    weather_highC = None
+    weather_lowC = None
+"""
